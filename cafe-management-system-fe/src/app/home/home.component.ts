d7da3ffd70c985153553adb07b74b302d88546ae
GeorgeQuantAnalyst <jiri.tester@outlook.com>
2023-10-25 21:11:04 +0200

    Implemented Forgot password page

--- cafe-management-system-fe/src/app/home/home.component.ts
+++ cafe-management-system-fe/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
+import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
 
 @Component({
   selector: 'app-home',
@@ -19,5 +20,12 @@ export class HomeComponent implements OnInit {
     dialogConfig.width = "550px";
     this.dialog.open(SignupComponent, dialogConfig);
   }
+
+  handleForgotPasswordAction() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = "550px";
+    this.dialog.open(ForgotPasswordComponent, dialogConfig);
+  }
+
 
 }
