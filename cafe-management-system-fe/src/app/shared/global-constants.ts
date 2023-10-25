d4d90530252b989a2b9ec4f132501acf961666b8
GeorgeQuantAnalyst <jiri.tester@outlook.com>
2023-10-25 19:04:43 +0200

    Implemented signup page

---
+++ cafe-management-system-fe/src/app/shared/global-constants.ts
@@ -0,0 +1,16 @@
+export class GlobalConstants{
+
+    //Message
+    public static genericError: string = "Something went wrong. Please try again later";
+
+    //Regex
+    public static nameRegex:string = "[a-zA-Z0-9 ]*";
+
+    public static emailRegex:string = "^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$";
+
+    public static contactNumberRegex:string = "^[e0-9]{10,10}$";
+
+    //Variable
+    public static error:string = "error";
+
+}
