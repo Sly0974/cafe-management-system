1cb78b1597dec43705e6e544c36cba99aa52be9e
GeorgeQuantAnalyst <jiri.tester@outlook.com>
2023-10-26 09:17:17 +0200

    Fixed ResponseMessage template in CafeUtils

--- cafe-management-system-be/src/main/java/com/example/cafemanagementsystem/util/CafeUtils.java
+++ cafe-management-system-be/src/main/java/com/example/cafemanagementsystem/util/CafeUtils.java
@@ -14,7 +14,7 @@ public class CafeUtils {
     }
 
     public static ResponseEntity<String> getResponseEntity(String responseMessage, HttpStatus httpStatus) {
-        return new ResponseEntity<String>("{\"message:\":\"" + responseMessage + "\"}", httpStatus);
+        return new ResponseEntity<String>("{\"message\":\"" + responseMessage + "\"}", httpStatus);
     }
 
     public static String getUUID() {
