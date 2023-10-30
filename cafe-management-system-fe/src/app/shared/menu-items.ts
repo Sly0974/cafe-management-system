81314bf2bc4309500ce7721a10461e9ce5a333cb
GeorgeQuantAnalyst <jiri.tester@outlook.com>
2023-10-30 19:13:37 +0100

    Implemented Category Page

--- cafe-management-system-fe/src/app/shared/menu-items.ts
+++ cafe-management-system-fe/src/app/shared/menu-items.ts
@@ -1,20 +1,22 @@
 import { Injectable } from "@angular/core";
 
-export interface Menu{
-    state:string;
-    name:string;
-    type:string;
-    icon:string;
-    role:string;
+export interface Menu {
+    state: string;
+    name: string;
+    type: string;
+    icon: string;
+    role: string;
 }
 
 const MENUITEMS = [
-    {state:'dashboard', name: 'Dashboard', type: 'link', icon:'dashboard', role: ''}
+    { state: 'dashboard', name: 'Dashboard', type: 'link', icon: 'dashboard', role: '' },
+    { state: 'category', name: 'Manage Category', type: 'link', icon: 'category', role: 'admin' }
+
 ]
 
 @Injectable()
-export class MenuItems{
-    getMenuItem():Menu[]{
+export class MenuItems {
+    getMenuItem(): Menu[] {
         return MENUITEMS;
     }
 }
