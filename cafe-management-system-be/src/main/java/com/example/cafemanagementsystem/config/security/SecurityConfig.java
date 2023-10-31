d7b4d87a73efb8cc0434751011511f980e5211d2
GeorgeQuantAnalyst <jiri.tester@outlook.com>
2023-10-31 10:54:56 +0100

    Fixed allow CORS for all HTTP methods
    - Spring framework by default set all origins for GET, HEAD, and POST methods. For other methods (example PUT, DELETE) you must explicit configuration.

--- cafe-management-system-be/src/main/java/com/example/cafemanagementsystem/config/security/SecurityConfig.java
+++ cafe-management-system-be/src/main/java/com/example/cafemanagementsystem/config/security/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -45,7 +47,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+        http.cors().configurationSource(request -> {
+                final CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+                corsConfiguration.setAllowedMethods(List.of("HEAD","GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+                return corsConfiguration;
+            })
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
@@ -62,5 +68,16 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer(){
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods("HEAD","GET","POST","PUT","DELETE","PATCH","OPTIONS");
+//            }
+//        };
+//    }
 
 }
